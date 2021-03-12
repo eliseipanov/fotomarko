6d392662bb88aa25d85fa0cdc5e9852f50d22e42
eliseipanov <eliseipanov@gmail.com>
2021-03-12 19:08:43 +0100

    text index without includes

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,17 +1,1 @@
-@include('layouts.header')
-<body>
-@include('layouts.navtop')
 
-<main id="main">
-@include('layouts.about')
-@include('layouts.clients')
-@include('layouts.services')
-@include('layouts.potrfolio')
-@include('layouts.footer')
-</main>
-<!-- End #main -->
-
-@include('layouts.footerjs')
-</body>
-
-</html>
