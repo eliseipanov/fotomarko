401ec5f8264987f7779b9a86d02c47d95f11f315
eliseipanov <eliseipanov@gmail.com>
2021-03-12 18:14:06 +0100

    Creates chanks for main index blade template

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+ <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Oleksii Marchenko - Forografer im Osnabrück</title>
+  <meta content="Нужен фотограф для компании? Альбом с воспоминаниями вместо мыльных фоток в Фейсбуке? НОМЕР ТЕЛЕФОНА" name="description">
+  <meta content="фотограф, русский фотограф в оснабрюке, русскогвоорящий фотограф, украинцы в Оснабрюке" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+@if ( {{@$someindexpagevariable}} )
+@include('layouts.header')
+@endif
+
+  <!-- =======================================================
+  * Template Name: Sailor - v4.0.1
+  * Template URL: https://bootstrapmade.com/sailor-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
