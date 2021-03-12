ea2fe1bb33f84be7f0733fa0367d71ee222ed6e9
eliseipanov <eliseipanov@gmail.com>
2021-03-12 20:14:19 +0100

    Asset naming six

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,22 +10,22 @@
   <meta content="фотограф, русский фотограф в оснабрюке, русскогвоорящий фотограф, украинцы в Оснабрюке" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{assets('assets')}}/img/favicon.png" rel="icon">
-  <link href="{{assets('assets')}}/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="{{asset('assets')}}/img/favicon.png" rel="icon">
+  <link href="{{asset('assets')}}/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{assets('assets')}}/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="{{assets('assets')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="{{assets('assets')}}/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="{{assets('assets')}}/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="{{assets('assets')}}/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="{{assets('assets')}}/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="{{assets('assets')}}/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
   <!-- Template Main CSS File -->
-  <link href="{{assets('assets')}}/css/style.css" rel="stylesheet">
+  <link href="{{asset('assets')}}/css/style.css" rel="stylesheet">
 
 
 <!--@include('layouts.flipbook') -->
