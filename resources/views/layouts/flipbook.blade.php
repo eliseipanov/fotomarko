7cc38144447fc527fba5b511b00c49487d4d5499
eliseipanov <eliseipanov@gmail.com>
2021-03-12 20:15:06 +0100

    Asset naming fix more

--- resources/views/layouts/flipbook.blade.php
+++ resources/views/layouts/flipbook.blade.php
@@ -1,3 +1,3 @@
-<script src="{{ assets('assets')}}/js/flipbook.min.js"></script>
+<script src="{{ asset('assets')}}/js/flipbook.min.js"></script>
 
-<script type="text/javascript" src="{{assets('assets')}}/js/fliponit.js"></script>
+<script type="text/javascript" src="{{asset('assets')}}/js/fliponit.js"></script>
