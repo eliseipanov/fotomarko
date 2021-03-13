f21761ecaa9682a6e368acbdc9a106e859c21b82
eliseipanov <eliseipanov@gmail.com>
2021-03-13 18:14:41 +0100

    Index Page Blocks order changed

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -4,10 +4,11 @@
 @include('layouts.navtop')
 
 <main id="main">
+@include('layouts.contact')
 @include('layouts.about')
-@include('layouts.clients')
-@include('layouts.services')
 @include('layouts.portfolio')
+@include('layouts.services')
+@include('layouts.clients')
 @include('layouts.footer')
 </main>
 <!-- End #main -->
