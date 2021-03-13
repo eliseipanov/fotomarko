08f13d8700e662912029c0056ed855e695eac971
eliseipanov <eliseipanov@gmail.com>
2021-03-13 17:56:49 +0100

    Images retossed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -40,10 +40,10 @@ $(document).ready(function () {
     $("#container").flipBook({
         pages:[
             {src:"https://fotomarko.de/assets/images/book2/page1.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb1.jpg", title:"fotomarko.de Book"},
-            {src:"https://fotomarko.de/assets/images/book2/page8.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb2.jpg", title:"Page two"},
-            {src:"https://fotomarko.de/assets/images/book2/page9.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb3.jpg", title:"Page three"},
-            {src:"https://fotomarko.de/assets/images/book2/page3.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb4.jpg", title:""},
-            {src:"https://fotomarko.de/assets/images/book2/page7.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb5.jpg", title:"Page five"},
+            {src:"https://fotomarko.de/assets/images/book2/page9.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb2.jpg", title:"Page two"},
+            {src:"https://fotomarko.de/assets/images/book2/page8.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb3.jpg", title:"Page three"},
+            {src:"https://fotomarko.de/assets/images/book2/page7.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb4.jpg", title:""},
+            {src:"https://fotomarko.de/assets/images/book2/page3.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb5.jpg", title:"Page five"},
             {src:"https://fotomarko.de/assets/images/book2/page5.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb6.jpg", title:"Page six"},
             {src:"https://fotomarko.de/assets/images/book2/page6.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb7.jpg", title:"Page seven"},
             {src:"https://fotomarko.de/assets/images/book2/page4.jpg", thumb:"https://fotomarko.de/assets/images/book2/thumb8.jpg", title:"Last"}
