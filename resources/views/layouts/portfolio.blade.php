6cde4174c3af36357bbd22c723a2c8082737b6e8
eliseipanov <eliseipanov@gmail.com>
2021-03-14 18:03:20 +0100

    Changed Subj category more

--- resources/views/layouts/portfolio.blade.php
+++ resources/views/layouts/portfolio.blade.php
@@ -152,12 +152,12 @@
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-6 portfolio-item filter-animals">
+        <div class="col-lg-4 col-md-6 portfolio-item filter-subjects">
           <div class="portfolio-wrap">
             <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
             <div class="portfolio-info">
-              <h4>Animals</h4>
-              <p>Card</p>
+              <h4>Subjects</h4>
+              <p>Naturmorte</p>
               <div class="portfolio-links">
                 <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" class="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i class="bx bx-link"></i></a>
