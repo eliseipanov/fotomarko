0d8043dfd35259f95b4ae6ab0477550ef87f7f41
eliseipanov <eliseipanov@gmail.com>
2021-03-29 08:26:19 +0200

    bootstrap script added

--- resources/views/layouts/blog/header.blade.php
+++ resources/views/layouts/blog/header.blade.php
@@ -36,6 +36,7 @@
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
  <script src="{{ asset('assets') }}/js/custom.js"></script>
 
 
