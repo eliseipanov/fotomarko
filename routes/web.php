a52c61a1007bed1c4c4ee7791adeb0bd8776a988
eliseipanov <eliseipanov@gmail.com>
2021-03-30 12:21:50 +0200

    Set locale somehow блядь

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\BlogController as BlogController;
 Route::group(['prefix' => '{locale}', 'where' => ['locale' => '[a-zA-Z]{2}']], function () {
 
    // App::setLocale($locale);
-   die(App::App::getLocale());
+   die(App::getLocale());
     Route::get('/', 'IndexPagecontroller@index');
     Route::get('blog', 'BlogController@index');
 });
