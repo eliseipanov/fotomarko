545722b2ad2583d153c6e3a3e10d4ec72bcc6a30
eliseipanov <eliseipanov@gmail.com>
2021-03-31 17:56:45 +0200

    About blade database records included 4

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -8,7 +8,7 @@
           <h3>{{ $blockabout[0]->description }}</h3>
         </div>
         <div class="col-lg-6 pt-4 pt-lg-0">
-          {{ $blockabout[0]->text }}
+          {!! $blockabout[0]->text !!}
           <ul>
             <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
             <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
