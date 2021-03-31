f9750f99561508311878c421560bf0263b102652
eliseipanov <eliseipanov@gmail.com>
2021-03-31 15:09:27 +0200

    Block about added

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,7 @@ class BlogController extends Controller
 {
     //
     public function index() {
+
     return view('layouts.blog.index');
     }
 }
--- app/Http/Controllers/IndexPageController.php
+++ app/Http/Controllers/IndexPageController.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Models\Blockabout;
 use Illuminate\Http\Request;
 
 class IndexPageController extends Controller
 {
     //
     public function index () {
-
-        return view('layouts.index');
+        $langnow = \App::getLocale();
+        $blockabout = Blockabout::withTranslations($langnow)->get();
+    return view('layouts.index', compact('blockabout'));
+        //return view('layouts.index');
     }
 }
