aa75809c8255e01d4ffb522575ac9a7f296a304f
eliseipanov <eliseipanov@gmail.com>
2021-04-02 19:22:07 +0200

    Lang switcher slash changes more 7

--- resources/views/layouts/blog/index.blade.php
+++ resources/views/layouts/blog/index.blade.php
@@ -318,7 +318,6 @@
       </div>
     </div>
   </footer><!-- End Footer -->
-  @include('cookieConsent::index')
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
