3eb90f3e799de1bd676a0fca8f5fe82f9f58ea02
eliseipanov <eliseipanov@gmail.com>
2021-04-02 20:08:35 +0200

    Designed by changes

--- resources/views/layouts/blog/index.blade.php
+++ resources/views/layouts/blog/index.blade.php
@@ -314,7 +314,7 @@
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> && Redisigned by <a href="https://buddha.net.ua/">www.buddha.net.ua</a>
       </div>
     </div>
   </footer><!-- End Footer -->
