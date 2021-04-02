fb92788750938004afa54a05fcf3c4b468477213
eliseipanov <eliseipanov@gmail.com>
2021-04-02 19:06:01 +0200

    Lang switcher slash changes

--- resources/views/layouts/navtop.blade.php
+++ resources/views/layouts/navtop.blade.php
@@ -38,8 +38,13 @@
           <li><a href="#contact" class="getstarted">{{ __('mainpage.getintouch') }}</a></li>
           <li class="dropdown"><a href="#"><span>{{ __('mainpage.language') }} </span> <i class="bi bi-chevron-right"></i></a>
             <ul>
-              <li><a href="{{ route('enhome') }}/{{ Request::segment(2)}}/{{ Request::segment(3) ?? '' }}"><span class="flag-icon flag-icon-us"> </span> English</a></li>
-              <li><a href="{{ route('dehome') }}/{{ Request::segment(2)}}/{{ Request::segment(3) ?? '' }}"><span class="flag-icon flag-icon-de"> </span> Deutsch</a></li>
+                @php
+                if(isset(Request::segment(2)) {$sla = "/";}
+                if(isset(Request::segment(3)) {$sla2 = "/";}
+                else {$sla = $sla2 = '';}
+                @endphp
+              <li><a href="{{ route('enhome') }}{{ $sla }}{{ Request::segment(2) ?? ''}}{{ $sla2 }}{{ Request::segment(3) ?? '' }}"><span class="flag-icon flag-icon-us"> </span> English</a></li>
+              <li><a href="{{ route('dehome') }}/{{ Request::segment(2) ?? ''}}/{{ Request::segment(3) ?? '' }}"><span class="flag-icon flag-icon-de"> </span> Deutsch</a></li>
             </ul>
           </li>
 
