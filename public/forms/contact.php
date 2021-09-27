e8906bf94214238577e2d6a3d64d39ecbdaa0ae0
eliseipanov <eliseipanov@gmail.com>
2021-09-27 18:45:19 +0200

    email code changed

--- public/forms/contact.php
+++ public/forms/contact.php
@@ -9,6 +9,8 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'master@buddha.net.ua';
 
+
+  /*
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
@@ -17,7 +19,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-
+*/
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
@@ -31,11 +33,44 @@
     'password' => 'pass',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send(); */
+  $to = $receiving_email_address;
+$subject = "From fotomarko.de website";
+
+$message = "
+<html>
+<head>
+<title>Свяжитесь с клиентом сайта</title>
+</head>
+<body>
+<p>Данные для связи:</p>
+<table>
+<tr>
+<th>Имя</th>
+<th>Фамилия</th>
+</tr>
+<tr>
+<td>".$_POST['name']."</td>
+<td>".$_POST['email']."</td>
+</tr>
+</table><HR> Заголовок:<BR/> ".$_POST['subject']."<br/>Месседж: <BR />".$_POST['message']."
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <www-data@fotomarko.de>' . "\r\n";
+$headers .= 'Cc: master@buddha.net.ua' . "\r\n";
+
+mail($to,$subject,$message,$headers);
 ?>
