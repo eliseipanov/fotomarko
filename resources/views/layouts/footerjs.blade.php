add0a6a0255b0944beb0b72b6d6dce1c8dbe2c99
eliseipanov <eliseipanov@gmail.com>
2021-09-27 18:57:37 +0200

    email code changed 4

--- resources/views/layouts/footerjs.blade.php
+++ resources/views/layouts/footerjs.blade.php
@@ -2,7 +2,7 @@
  <script src="{{asset('assets')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
  <script src="{{asset('assets')}}/vendor/glightbox/js/glightbox.min.js"></script>
  <script src="{{asset('assets')}}/vendor/isotope-layout/isotope.pkgd.min.js"></script>
- <script src="{{asset('assets')}}/vendor/php-email-form/validate.js"></script>
+ {{-- <script src="{{asset('assets')}}/vendor/php-email-form/validate.js"></script> --}}
  <script src="{{asset('assets')}}/vendor/swiper/swiper-bundle.min.js"></script>
  <script src="{{asset('assets')}}/vendor/waypoints/noframework.waypoints.js"></script>
 
