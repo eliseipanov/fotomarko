4651772c687c47d914a617aa537fca1f7dc42eef
eliseipanov <eliseipanov@gmail.com>
2021-09-27 18:14:36 +0200

    Footer changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,29 +6,29 @@
 
           <div class="col-lg-3 col-md-6">
             <div class="footer-info">
-              <h3>MARKO GMBH</h3>
+              <h3> OLEKSII MARCHENKO</h3>
               <p>
-                A108 Adam Street <br>
-                NY 535022, USA<br><br>
-                <strong>Phone:</strong> +1 5589 55488 55<br>
-                <strong>Email:</strong> info@example.com<br>
+                Bremer Str. 175 <br>
+                49086, Osnabrück<br><br>
+                <strong>Phone:</strong> +49 176 77975978<br>
+                <strong>Email:</strong> oleksii.pmarchenko@gmail.com<br>
               </p>
               <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                <a href="https://twitter.com/buddhanetua" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="https://www.facebook.com/oleksa.mara/" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="https://www.instagram.com/masterofsilk/" class="instagram"><i class="bx bxl-instagram"></i></a>
+                {{-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> --}}
+                {{-- <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> --}}
               </div>
             </div>
           </div>
 
           <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
+            <h4>Links</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+            {{--   <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li> --}}
               <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
             </ul>
@@ -37,17 +37,17 @@
           <div class="col-lg-3 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://buddha.net.ua">Main Blog</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://www.facebook.com/groups/ukraine.photographers">Ukrainian Foto-verein</a></li>
+              {{-- <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li> --}}
             </ul>
           </div>
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <h4>E-Mail an mich</h4>
+            <p>Ich werde Sie in der nächsten Arbeitsstunde kontaktieren</p>
             <form action="" method="post">
               <input type="email" name="email"><input type="submit" value="Subscribe">
             </form>
@@ -60,14 +60,14 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong><span>Sailor</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Oleksii Piliuhin-Marchenko</span></strong>. 2018 - All Rights Reserved
       </div>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->
-        Designed by <a href="bootstrapmade.com">BootstrapMade</a> && Redisigned by <a href="https://buddha.net.ua/">www.buddha.net.ua</a>
+        Designed by <a href="bootstrapmade.com">Oleksii Piliuhin-Marchenko</a> && Website <a href="https://buddha.net.ua/">www.buddha.net.ua</a>
       </div>
     </div>
   </footer><!-- End Footer -->
